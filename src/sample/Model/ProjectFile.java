30da124f256d39a0341e60f296626109268686e1
Michael Bui <michaelbui99@hotmail.com>
2020-12-08 19:28:59 +0100

    ProjectFile kalder nu de korrekte metoder ved læsning og skrivning af filer

--- src/sample/Model/ProjectFile.java
+++ src/sample/Model/ProjectFile.java
@@ -2,7 +2,7 @@ package sample.Model;
 
 import java.io.*;
 
-public class ProjectFile
+public class ProjectFile implements Serializable
 {
   //Fields
   private String fileName;
@@ -20,14 +20,14 @@ public class ProjectFile
     ProjectList projectList = new ProjectList(); //Bruges som return for metoden.
     FileInputStream fis = null;
     ObjectInputStream in = null;
-    fis = new FileInputStream(fileName) //Angiver path til filen
+    fis = new FileInputStream(fileName); //Angiver path til filen
     in = new ObjectInputStream(fis);
     int counter = in.readInt();
     for (int i = 0; i < counter; i++)
     {
       if (in.readObject() instanceof Project) //Checker om objektet vi læser er et Project object inden vi caster.
       {
-        projectList.add((Project) in.readObject());
+        projectList.addProject((Project) in.readObject());
       }
     }
     in.close();
@@ -48,29 +48,11 @@ public class ProjectFile
     {
       if (in.readObject() instanceof TeamMember)
       /*
-      * Vi Checker hvilken class vi prøver at læse, og caster afhængig af hvilken subclass.
+      * Vi Checker om vi prøver at læse et TeamMember object før vi caster.
       * */
       {
-        employeeList.add((TeamMember) in.readObject());
+        employeeList.addEmployee((TeamMember) in.readObject());
       }
-      else if(in.readObject()instanceof Admin)
-      {
-        employeeList.add((Admin) in.readObject());
-      }
-      else if (in.readObject() instanceof ScrumMaster)
-      {
-        employeeList.add((ScrumMaster) in.readObject());
-      }
-      else if (in.readObject() instanceof ProductOwner)
-      {
-        employeeList.add((ProductOwner) in.readObject());
-      }
-      else if (in.readObject() instanceof ProjectCreator)
-      {
-        employeeList.add((ProjectCreator) in.readObject());
-      }
-
-
     }
 
     in.close();
@@ -84,14 +66,14 @@ public class ProjectFile
     TaskList taskList = new TaskList(); //Bruges som return for metoden.
     FileInputStream fis = null;
     ObjectInputStream in = null;
-    fis = new FileInputStream(fileName) //Angiver path til filen
+    fis = new FileInputStream(fileName); //Angiver path til filen
     in = new ObjectInputStream(fis);
     int counter = in.readInt();
     for (int i = 0; i < counter; i++)
     {
       if (in.readObject() instanceof Task) //Checker om objektet vi læser er et Task object inden vi caster.
       {
-        projectList.add((Task) in.readObject());
+        taskList.addTask((Task) in.readObject());
       }
     }
     in.close();
@@ -106,14 +88,14 @@ public class ProjectFile
     RequirementList requirementList = new RequirementList(); //Bruges som return for metoden.
     FileInputStream fis = null;
     ObjectInputStream in = null;
-    fis = new FileInputStream(fileName) //Angiver path til filen
+    fis = new FileInputStream(fileName); //Angiver path til filen
     in = new ObjectInputStream(fis);
     int counter = in.readInt();
     for (int i = 0; i < counter; i++)
     {
-      if (in.readObject() instanceof Project) //Checker om objektet vi læser er et Project object inden vi caster.
+      if (in.readObject() instanceof Requirement) //Checker om objektet vi læser er et Requirement object inden vi caster.
       {
-        projectList.add((Project) in.readObject());
+        requirementList.addRequirement((Requirement) in.readObject());
       }
     }
     in.close();
@@ -124,6 +106,10 @@ public class ProjectFile
 
 
   //Setters
+
+
+
+
   public void writeProjectListFile(ProjectList projectList) throws IOException
 
   {
@@ -133,10 +119,10 @@ public class ProjectFile
 
     fos = new FileOutputStream(file);
     out = new ObjectOutputStream(fos);
-    out.writeInt(projectList.size());
-    for (int i = 0; i < projectList.size(); i++)
+    out.writeInt(projectList.getAllProjects().size());
+    for (int i = 0; i < projectList.getAllProjects().size(); i++)
     {
-      out.writeObject(projectList.get(i));
+      out.writeObject(projectList.getAllProjects().get(i));
       out.flush();
     }
 
@@ -152,10 +138,10 @@ public class ProjectFile
 
     fos = new FileOutputStream(file);
     out = new ObjectOutputStream(fos);
-    out.writeInt(employeeList.size());
-    for (int i = 0; i < employeeList.size(); i++)
+    out.writeInt(employeeList.getAllEmployees().size());
+    for (int i = 0; i < employeeList.getAllEmployees().size(); i++)
     {
-      out.writeObject(employeeList.get(i));
+      out.writeObject(employeeList.getEmployee(i));
       out.flush();
     }
 
@@ -170,10 +156,10 @@ public class ProjectFile
 
     fos = new FileOutputStream(file);
     out = new ObjectOutputStream(fos);
-    out.writeInt(taskList.size());
-    for (int i = 0; i < taskList.size(); i++)
+    out.writeInt(taskList.getAllTasks().size());
+    for (int i = 0; i < taskList.getAllTasks().size(); i++)
     {
-      out.writeObject(taskList.get(i));
+      out.writeObject(taskList.getAllTasks().get(i));
       out.flush();
     }
 
@@ -188,10 +174,10 @@ public class ProjectFile
 
     fos = new FileOutputStream(file);
     out = new ObjectOutputStream(fos);
-    out.writeInt(requirementList.size());
-    for (int i = 0; i < requirementList.size(); i++)
+    out.writeInt(requirementList.getAllRequirements().size());
+    for (int i = 0; i < requirementList.getAllRequirements().size(); i++)
     {
-      out.writeObject(requirementList.get(i));
+      out.writeObject(requirementList.getAllRequirements().get(i));
       out.flush();
     }
 

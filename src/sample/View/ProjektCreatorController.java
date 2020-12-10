60713e304be38dd1adaf7b983d3e2e96587d539d
Kutaiba-Nezar-Kashmar <kkashmar94.kk@gmail.com>
2020-12-10 13:06:56 +0100

    GUI FXML thinges

--- src/sample/View/ProjektCreatorController.java
+++ src/sample/View/ProjektCreatorController.java
@@ -2,6 +2,7 @@ package sample.View;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.Region;
 import javafx.stage.Stage;
 import sample.Model.ManagementSystemModel;
@@ -10,6 +11,20 @@ public class ProjektCreatorController
 {
   @FXML private Button cancel;
   @FXML private Button accept;
+  @FXML private Button create;
+  @FXML private Button add;
+  @FXML private Button remove;
+  @FXML private TextField inputProjectName;
+  @FXML private TextField inputProjectID;
+  @FXML private TextField inputDay;
+  @FXML private TextField inputMonth;
+  @FXML private TextField inputYear;
+  @FXML private TextField inputTeamMemberName;
+  @FXML private TextField inputTeamMemberRole;
+  @FXML private TextField inputAssignTeamMemberToProject;
+  @FXML private TextField inputAssignTeamMemberToAssignment;
+  @FXML private TextField inputRemovalTeamMemberFromProject;
+  @FXML private TextField inputProjectForTeamMemberRemoval;
 
   private ManagementSystemModel model;
   private ViewHandler viewHandler;

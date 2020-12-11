e87a1192fbd8c26899ec257ceac90d0f152a3937
Kutaiba-Nezar-Kashmar <kkashmar94.kk@gmail.com>
2020-12-11 13:33:34 +0100

    GUI FXML thinges

--- src/sample/View/ScrumMasterController.java
+++ src/sample/View/ScrumMasterController.java
@@ -8,55 +8,6 @@ import sample.Model.*;
 
 public class ScrumMasterController
 {
- /* @FXML
-  private Button cancel;
-  @FXML
-  private TextField responsibleMember;
-
-  @FXML
-  private TextField projectName;
-
-  @FXML
-  private Button chooseProjectBtn;
-
-  @FXML
-  private TextField reqID;
-
-  @FXML
-  private Button chooseReqBtn;
-
-  @FXML
-  private Button approveChangesBtn;
-
-  @FXML
-  private ToggleGroup reqPriority;
-
-  @FXML
-  private ToggleGroup reqStatus;
-
-  @FXML
-  private TextField taskName;
-
-  @FXML
-  private Button addTaskBtn;
-
-  @FXML
-  private TextField taskID;
-
-  @FXML
-  private Label responsibleError;
-
-  @FXML
-  private ToggleGroup taskPriority;
-
-  @FXML
-  private TextField deadlineDay;
-
-  @FXML
-  private TextField deadlineYear;
-
-  @FXML
-  private TextField deadlineMonth;*/
 
   @FXML private ChoiceBox requirementPriority;
   @FXML private ChoiceBox requirementStatus;
@@ -110,39 +61,6 @@ public class ScrumMasterController
     Stage stage = (Stage) cancel.getScene().getWindow();
     stage.close();
   }
-
-
-  /*public void chooseProject()
-  {
-    if (projectName.getText() != null || projectName.getText()!= "")
-    {
-      chosenProject = model.getProjectList().getProject(projectName.getText());
-    }
-    else
-    projectName.setPromptText("ERROR TRY AGAIN");
-  }
-
-  public void chooseRequirement()
-  {
-    chosenRequirement = chosenProject.getRequirementList().getRequirement(Integer.parseInt(reqID.getText()));
-  }
-
-  public void editRequirement()
-  {
-    RadioButton selectedReqPriority = (RadioButton) reqPriority.getSelectedToggle();
-    chosenRequirement.setPriority(selectedReqPriority.getText());
-
-    RadioButton selectedReqStatus = (RadioButton) reqStatus.getSelectedToggle();
-    chosenRequirement.setStatus(selectedReqStatus.getText());
-  }
-
-  public void addTask()
-  {
-    RadioButton selectedTaskPrio = (RadioButton) taskPriority.getSelectedToggle();
-    String responsibleName = responsibleMember.getText();
-    TeamMember responsibleMember = chosenRequirement.getEmployees().getEmployee(responsibleName);
-    chosenRequirement.getTaskList().addTask(new Task(taskName.getText(),Integer.parseInt(taskID.getText()),"Ikke påbegyndt",responsibleMember,selectedTaskPrio.getText(),new MyDate(Integer.parseInt(deadlineDay.getText()),Integer.parseInt(deadlineMonth.getText()),Integer.parseInt(deadlineYear.getText()))));
-  }*/
 
   public void setRequirementPriority()
   {
